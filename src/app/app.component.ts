ccaadba09e6cb51a40a723d3033a3e6716ddc62e
CuongNM <cuong.nm172989@sis.hust.edu.vn>
2022-01-07 22:27:58 +0700

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tikiComment';
+  name=""
+}
